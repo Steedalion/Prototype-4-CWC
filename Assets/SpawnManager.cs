def53905697ec2b3700821b1fa20786a17f15d74
STEEDCLINT ALEX <steed@unist.ac.kr>
2020-12-28 16:41:06 +0900

    Enemies spawn at different positions.

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+	public float spawnMin, spawnMax;
+	WaitForSeconds wait;
+	float delay = 5;
+    // Start is called before the first frame update
+    void Start()
+	{
+		wait = new WaitForSeconds(delay);
+	    StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	IEnumerator Spawn()
+	{
+		while (true){}
+		Vector3 spawnPos = new Vector3(Random.Range(spawnMin, spawnMax), Random.Range(spawnMin, spawnMax), Random.Range(spawnMin, spawnMax));
+		Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+		yield return wait;
+	}
+}
