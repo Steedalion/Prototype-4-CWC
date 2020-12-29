84337cc30900d16067c5d546d3df2eea52d93474
STEEDCLINT ALEX <steed@unist.ac.kr>
2020-12-29 12:38:38 +0900

    Prototype complete

---
+++ Assets/DestroyEnemiesThatEnterTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemiesThatEnterTrigger : MonoBehaviour
+{
+	public SpawnManager enemySpawner;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	// OnTriggerEnter is called when the Collider other enters the trigger.
+	protected IEnumerator OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Enemy"))
+		{
+			Destroy(other.gameObject);
+			yield return null;
+			enemySpawner.UpdateEnemyCount();
+		}
+		
+		yield return null;
+	}
+}
--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -8,25 +8,47 @@ public class SpawnManager : MonoBehaviour
 	public float spawnMin, spawnMax;
 	WaitForSeconds wait;
 	float delay = 5;
+	int enemyCount = 0;
+	int waveNumber =1;
+	public GameObject powerupPrefab;
     // Start is called before the first frame update
     void Start()
 	{
 		wait = new WaitForSeconds(delay);
-	    StartCoroutine(Spawn());
+		SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	    
     }
-    
-	IEnumerator Spawn()
+	void SpawnEnemyWave(int enemiesToSpawn)
 	{
-		while (true){
-			Vector3 spawnPos = new Vector3(Random.Range(spawnMin, spawnMax), Random.Range(0, spawnMax), Random.Range(spawnMin, spawnMax));
-			Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+		for (int i = 0; i < enemiesToSpawn; i++) {
+			StartCoroutine(Spawn(enemyPrefab));
+		}
+		StartCoroutine(Spawn(powerupPrefab));
+	}
+    
+	IEnumerator Spawn(GameObject prefab)
+	{
+	
+		Vector3 spawnPos = new Vector3(Random.Range(spawnMin, spawnMax), 0, Random.Range(spawnMin, spawnMax));
+			Instantiate(prefab, spawnPos, Quaternion.identity);
 			yield return wait;
+		
+	}
+	
+	public void UpdateEnemyCount()
+	{
+		enemyCount = FindObjectsOfType<MoveTowardPlayer>().Length;
+		Debug.Log("Enemies left "+enemyCount);
+		Debug.Log("Enemies left "+FindObjectOfType<MoveTowardPlayer>());
+		if (enemyCount == 0)
+		{
+			waveNumber++;
+			SpawnEnemyWave(waveNumber);
 		}
 	}
 }
